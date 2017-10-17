1d7028ab71b5214192508bfe1b328ff985febfab
Al Zimmerman <auraelius@gmail.com>
2017-10-17 12:00:18 -0700

    Set up bootstrapping and main app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,38 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-
+import { AngularFireAuth } from 'angularfire2/auth';
 import { HomePage } from '../pages/home/home';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  // When the user opens the app, it does not go straight to the HomePage, it should listen to the authentication state and then decide where to take the user.
+  rootPage:any;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform, 
+    statusBar: StatusBar, 
+    splashScreen: SplashScreen,
+    afAuth: AngularFireAuth
+  ) {
+    // The subscribe function will listen for authentication changes, if there’s a logged-in user the app will send her to the HomePage if not, she will get redirected to the LandingPage where she will be able to either log-in or start using the app anonymously.
+
+    // Did you notice that we are assigning the home page by its class name, but the login page is a string? That is because of how code splitting works, all of our pages, except HomePage, are declared in their page modules. To call those pages, we need to use the string name they have, by default, that name is the same their class name.
+
+    // it will stop listening to the authentication state unless you run it again (it runs every time someone opens the app). because if there’s a sudden change in the authentication state (like when we link to an email or if user changes their email or password) this function would trigger.
+
+    const authListener = afAuth.authState.subscribe(user => {
+      if (user) {
+        this.rootPage = HomePage;
+        authListener.unsubscribe();
+      } else {
+        this.rootPage = 'LandingPage';
+        authListener.unsubscribe();
+      }
+    });
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,23 +27,29 @@ class CameraMock extends Camera {
   }
 }
 
+// An NgModule class describes how the application parts fit together. Every application has at least one NgModule, the root module that you bootstrap to launch the application. You can call it anything you want. The conventional name is AppModule. @NgModule takes a metadata object that tells Angular how to compile and launch the application. 1) imports — the BrowserModule that this and every application needs to run in a browser. 2) declarations — the application's lone component, which is also ... 3) bootstrap — the root component that Angular creates and inserts into the index.html host web page.
+
 @NgModule({
-  declarations: [
+  // You tell Angular which components belong to the AppModule by listing it in the module's declarations array. Only declarables — components, directives and pipes — belong in the declarations array. Do not put any other kind of class in declarations; not NgModule classes, not service classes, not model classes.
+  declarations: [ 
     MyApp,
     HomePage
   ],
-  imports: [
+  // Only NgModule classes go in the imports array
+  imports: [ 
     BrowserModule,
     IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(firebaseConfig), 
     AngularFireAuthModule, 
     AngularFireDatabaseModule
   ],
+  // the root component that Angular creates and inserts into the index.html host web page.  Among other things, the bootstrapping process creates the component(s) listed in the bootstrap array and inserts each one into the browser DOM. Each bootstrapped component is the base of its own tree of components. Inserting a bootstrapped component usually triggers a cascade of component creations that fill out that tree.
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage
   ],
+  //  If you add a provider to the bootstrap then a single instance of the service will be created that all components in the application will share (https://www.joshmorony.com/an-in-depth-explanation-of-providers-in-ionic-2/ ) You must import the provider into app.module.ts and add it to the providers array. You must import the provider into any component that uses it. You must inject the provider in the constructor of any component that is using it
   providers: [
     StatusBar,
     SplashScreen,
