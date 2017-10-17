d18ca874ed4bf59bdcc48f6a401ba99b16ee9386
Al Zimmerman <auraelius@gmail.com>
2017-10-17 10:41:28 -0700

    Add camera and mock, first build.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,24 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database'; 
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { firebaseConfig } from './credentials';
+import { Camera } from '@ionic-native/camera';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { AuthProvider } from '../providers/auth/auth';
 import { BillProvider } from '../providers/bill/bill';
+
+// remove this when testing in devices
+class CameraMock extends Camera {
+  getPicture(options) {
+    return new Promise((resolve, reject) => {
+      resolve(`iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAIAAAAC64paAAAABmJLR0QAA
+        AAAAAD5Q7t/AAAACXBIWXMAAA7EAAAOxAGVKw4bAAAAUklEQVR4nGM4QAFgAOL/uMG
+        7d+8gJCZAaMZrARaArhmXzVjtpLtmiAiZmiH6ydf8jhKbqaoZzscEWE2E2zeqeWA0k
+        wRQNOMCWG2G28+A1wICAAAS2y1ybs6L0AAAAABJRU5ErkJggg==`);
+    });
+  }
+}
 
 @NgModule({
   declarations: [
@@ -21,7 +34,7 @@ import { BillProvider } from '../providers/bill/bill';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
     AngularFireModule.initializeApp(firebaseConfig), 
     AngularFireAuthModule, 
     AngularFireDatabaseModule
@@ -34,6 +47,7 @@ import { BillProvider } from '../providers/bill/bill';
   providers: [
     StatusBar,
     SplashScreen,
+    { provide: Camera, useClass: CameraMock },
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     AuthProvider,
     BillProvider
